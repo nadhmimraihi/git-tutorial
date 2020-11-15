2b0d0b7ab4fcd15e869bd2e0b83c05e06ef9e506
Alex Hoffman <alxhoff@gmail.com>
2020-11-15 18:13:23 +0100

    coding challenge 2

--- src/MAIN FUNCTION.c
+++ src/MAIN FUNCTION.c
@@ -1,10 +1,25 @@
 
 #include <stdio.h>
+#include "espl_lib.h"
 
 int main(void)
 {
-    printf("Hello ESPL");
 
+int n;
+char c;
+int k=1;
+
+while(k==1){
+printf("enter a number : \n");
+scanf("%d", &n);
+printf("%s\n", num_to_words(n));
+printf("enter r to replay or q to quit : \n" );
+c= getchar();
+scanf("%c",&c);
+if (c == 'q') k =0;
+else if (c == 'r') k=1 ;
+
+}
 return 0  ;
    
 }
